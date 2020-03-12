f3dcb9842c4f8f570382ce8ec1fba0aa309efd87
jrlaguna <jrtlaguna@gmail.com>
2020-03-12 22:53:26 +0800

    management command for populating database

--- c130/management/commands/summon.py
+++ c130/management/commands/summon.py
@@ -23,66 +23,66 @@ class Command(BaseCommand):
         location_url = 'https://rickandmortyapi.com/api/location/'
         episode_url = 'https://rickandmortyapi.com/api/episode/'
 
-        # character_info = requests.get(character_url).json()
+        character_info = requests.get(character_url).json()
         
 
-        # for i in range(character_info['info']['count']):
+        for i in range(character_info['info']['count']):
             
-        #     character = requests.get(url=f'{character_url}{i+1}').json()
+            character = requests.get(url=f'{character_url}{i+1}').json()
 
-        #     if(Character.objects.filter(name=character['name']).exists()):
-        #         continue
+            if(Character.objects.filter(name=character['name']).exists()):
+                continue
 
-        #     print(f"Creating character {character['name']}")
-        #     instance = Character(
-        #         api_id = character['id'],
-        #         name = character['name'],
-        #         status = character['status'],
-        #         species = character['species'],
-        #         type = character['type'],
-        #         gender = character['gender'],
-        #         image= character['image'],
-        #         url = character['url'],
-        #         created = character['created']
-        #     )
+            print(f"Creating character {character['name']}")
+            instance = Character(
+                api_id = character['id'],
+                name = character['name'],
+                status = character['status'],
+                species = character['species'],
+                type = character['type'],
+                gender = character['gender'],
+                image= character['image'],
+                url = character['url'],
+                created = character['created']
+            )
             
-        #     origin = character['origin']['name']
-        #     location = character['location']['name']
+            origin = character['origin']['name']
+            location = character['location']['name']
 
-        #     if not Location.objects.filter(name=character['origin']['name']).exists():
-        #         origin = Location.objects.create(name=origin)
-        #     if not Location.objects.filter(name=character['location']['name']).exists():
-        #         location = Location.objects.create(name=location)
+            if not Location.objects.filter(name=character['origin']['name']).exists():
+                origin = Location.objects.create(name=origin)
+            if not Location.objects.filter(name=character['location']['name']).exists():
+                location = Location.objects.create(name=location)
 
-        #     origin = Location.objects.filter(name=origin)
-        #     location = Location.objects.filter(name=location)
+            origin = Location.objects.filter(name=origin)
+            location = Location.objects.filter(name=location)
 
 
-        #     instance.origin = origin.first()
-        #     instance.location = location.first()
-        #     instance.save()
+            instance.origin = origin.first()
+            instance.location = location.first()
+            instance.save()
 
         
-        # location_info = requests.get(location_url).json()
+        location_info = requests.get(location_url).json()
 
-        # for i in range(location_info['info']['count']):
+        for i in range(location_info['info']['count']):
 
-        #     location = requests.get(f'{location_url}{i+1}').json()
+            location = requests.get(f'{location_url}{i+1}').json()
             
             
 
-        #     if Location.objects.filter(name=location['name']).exists():
-        #         instance = Location.objects.filter(name=location['name']).first()
-        #         print(f"Updating {instance.name} info.")
-        #     else:
-        #         instance = Location.objects.create(name=location['name'])
-        #         print(f"Creating location {instance.name}")
+            if Location.objects.filter(name=location['name']).exists():
+                instance = Location.objects.filter(name=location['name']).first()
+                print(f"Updating {instance.name} info.")
+            else:
+                instance = Location.objects.create(name=location['name'])
+                print(f"Creating location {instance.name}")
 
-        #     instance.type = location['type']
-        #     instance.dimension = location['dimension']
-        #     instance.url = location['url']
-        #     instance.created = location['created']
-        #     instance.save()
+            instance.type = location['type']
+            instance.dimension = location['dimension']
+            instance.url = location['url']
+            instance.created = location['created']
+            instance.save()
 
         episode_info = requests.get(episode_url, headers=headers).json()
 
